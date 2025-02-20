95eac166159906696315acef5ebd794f94f5952e
shubhamdahitule <shubhamdahitule1@gmail.com>
2025-02-21 00:46:37 +0530

    Implemented DeliveryPerson class

---
+++ src/com/tns/fooddeliverysystem/entities/DeliveryPerson.java
@@ -0,0 +1,38 @@
+public class DeliveryPerson {
+    // Private fields
+    private int deliveryPersonId;
+    private String name;
+    private long contactNo;
+
+    // Constructor
+    public DeliveryPerson(int deliveryPersonId, String name, long contactNo) {
+        this.deliveryPersonId = deliveryPersonId;
+        this.name = name;
+        this.contactNo = contactNo;
+    }
+
+    // Getter methods
+    public int getDeliveryPersonId() {
+        return deliveryPersonId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getContactNo() {
+        return contactNo;
+    }
+
+    // Overriding toString() method
+    @Override
+    public String toString() {
+        return "DeliveryPerson{" +
+               "deliveryPersonId=" + deliveryPersonId +
+               ", name='" + name + '\'' +
+               ", contactNo=" + contactNo +
+               '}';
+    }
+
+   
+}
